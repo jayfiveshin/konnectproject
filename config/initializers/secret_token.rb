8defa9b8a279cf26d530bca4849249393eeeffbe
Joonha Shin <shin.joonha@gmail.com>
2011-11-13 15:58:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konnectproject::Application.config.secret_token = '233171de955d3684b67fef5709ad5f2a6212763d251eb360876cfb30dff31054a0a09d3a85e8f5de0d30371fe80fe5477b0422d7610f71213bcdd94a24364b93'
